b15092add715026090aaf178991a39be8b897ba9
AishwaryaGitay <aishwaryagitay@gmail.com>
2026-05-07 14:46:07 +0100

    Created spring boot project with the dependencies needed

---
+++ SpartaAcademy/src/main/java/com/sparta/spartaacademy/SpartaAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spartaacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaAcademyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaAcademyApplication.class, args);
+    }
+
+}
---
+++ SpartaAcademy/src/test/java/com/sparta/spartaacademy/SpartaAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spartaacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaAcademyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
